dbf19bcc66df49efa2d2d58276edfc22be9bc28e
acanois <david.richter42@gmail.com>
2024-11-13 13:43:49 -0600

    start auth setup

--- app/api.py
+++ app/api.py
@@ -1,10 +1,21 @@
-from fastapi import FastAPI
+"""API"""
+
+from typing import Annotated
+
+from fastapi import FastAPI, Depends
+from fastapi.security import OAuth2PasswordBearer
 from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-@app.get("/")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+
+@app.get('/')
 async def home():
-    message = {"message": "home"}
+    message = {'message': 'home'}
     
     return JSONResponse(message)
+
+@app.get('/items')
+async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
+    return {'token': token}
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,12 @@
+"""API Tests"""
+
 from fastapi.testclient import TestClient
 from app.api import app
 
 client = TestClient(app)
 
 def test_home():
-    response = client.get("/")
+    """Test the home page"""
+    response = client.get('/')
     assert response.status_code == 200
-    assert response.json() == {"message": "home"}
+    assert response.json() == {'message': 'home'}
