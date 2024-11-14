52b2e9e62a03623bd5d4af31ea40d79d2e2fb28f
acanois <david.richter42@gmail.com>
2024-11-14 14:20:35 -0600

    add jwt functions

---
+++ credentials.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.environ["SECRET_KEY"]
+ALGORITHM = os.environ["ALGORITHM"]
+EXPIRE_TIME_MINUTES = os.environ["EXPIRE_TIME_MINUTES"]
+
