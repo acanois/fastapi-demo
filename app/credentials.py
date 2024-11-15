de8117413a2fa4e4b6af7bd54c4e5b3466c20af7
acanois <david.richter42@gmail.com>
2024-11-15 13:13:16 -0600

    update env vars

--- app/credentials.py
+++ app/credentials.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-FAKE_SECRET_KEY = os.environ["SECRET_KEY"]
+FAKE_SECRET_KEY = os.environ["FAKE_SECRET_KEY"]
 EXPIRE_TIME_MINUTES = os.environ["EXPIRE_TIME_MINUTES"]
