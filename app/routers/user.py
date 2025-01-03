c09854a2468ec3a27bf3bc8e412a3072109a70de
acanois <david.richter42@gmail.com>
2025-01-03 13:30:40 -0600

    add get user

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,18 +1,19 @@
 """USER Router"""
 
 from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session
 
 from ..database import get_session
 from ..models.user import User
 
+session = Annotated[Session, Depends(get_session)]
 
 router = APIRouter(prefix="/user", tags=["user"])
 
 
-@router.post("/create")
-def create_user(user: User, session: Annotated[Session, Depends(get_session)]):
+@router.post("/create", response_model=User)
+def create_user(user: User, session: session):
     """Create a new user
 
     Args:
@@ -27,4 +28,24 @@ def create_user(user: User, session: Annotated[Session, Depends(get_session)]):
     session.commit()
     session.refresh(user)
 
+    return user
+
+@router.get("/{user_id}", response_model=User)
+def get_user_by_id(user_id: int, session: session):
+    """Get a single user by id
+
+    Args:
+        user_id (int): The user id to query
+        session (Annotated[Session, Depends): Session object for the transaction
+
+    Raises:
+        HTTPException: 404 if user not found
+
+    Returns:
+        user: User found by id
+    """
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
     return user
