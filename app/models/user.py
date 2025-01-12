7080dbe5117018d34a972ce8c3665f4ea807a95a
acanois <david.richter42@gmail.com>
2025-01-12 12:31:54 -0600

    add test_create_user

--- app/models/user.py
+++ app/models/user.py
@@ -3,8 +3,9 @@ from sqlmodel import SQLModel, Field
 
 class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    username: str = Field(index=True)
-    email: str = Field(index=True)
+    user_id: str = Field(index=True)
+    username: str = Field()
+    email: str = Field()
     first_name: str | None = Field(default=None)
     last_name: str | None = Field(default=None)
     active: bool
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,13 +1,56 @@
 """API Tests"""
 
+from uuid import uuid4
+
+import pytest
+from sqlmodel import SQLModel
+
+from typing import Annotated
+
+from fastapi import Depends
 from fastapi.testclient import TestClient
-from app.api import app
+
+from ..app.api import app
+from ..app.database import Session, get_session
+from ..app.models.user import User
 
 client = TestClient(app)
+
+
+@pytest.fixture
+def db_session():
+    return Annotated[Session, Depends(get_session)]
+
+@pytest.fixture
+def test_user():
+    user_id = uuid4()
+    new_user = {
+        "user_id": str(user_id),
+        "username": "test_user",
+        "email": "string",
+        "first_name": "test",
+        "last_name": "user",
+        "active": True
+    }
+
+    return new_user
 
 
 def test_home():
     """Test the home page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "home"}
+
+
+def test_create_user(db_session, test_user):
+    """Test user creation"""
+    new_user = User.model_validate(user_model)
+
+    db_session.add(new_user)
+    db_session.commit()
+    db_session.refresh(new_user)
+
+    user_from_db = db_session.get(User, user_id=test_user["user_id"])
+
+    assert user_from_db is not None
+    assert user_from_db["user_id"] == new_user["user_id"]
