cb70c397c63d7d088ea55029e4fd5147ae05739e
acanois <david.richter42@gmail.com>
2025-01-22 13:34:57 -0600

    remove user_id and add created and updated times

--- app/models/user.py
+++ app/models/user.py
@@ -1,10 +1,12 @@
+from datetime import datetime
 from typing import List
 from sqlmodel import SQLModel, Field
 
 
 class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    user_id: str = Field(index=True)
+    created_at: datetime = Field(default=datetime.now())
+    updated_at: datetime = Field(default=datetime.now())
     username: str = Field()
     email: str = Field()
     first_name: str | None = Field(default=None)
--- tests/test_user.py
+++ tests/test_user.py
@@ -1,16 +1,16 @@
 """API Tests"""
 
-from uuid import uuid4
-
 import pytest
-from sqlmodel import SQLModel, Session
+from sqlmodel import SQLModel, Session, text
 
 from fastapi.testclient import TestClient
 
 from ..app.api import app
 from ..app.database import engine, get_session
 
+
 SQLModel.metadata.create_all(engine)
+
 
 @pytest.fixture
 def test_client():
@@ -18,7 +18,7 @@ def test_client():
     def test_session():
         with Session(engine) as session:
             yield session
-            session.rollback()
+            session.exec(text("""TRUNCATE TABLE "user" RESTART IDENTITY;"""))
 
     app.dependency_overrides[get_session] = test_session
 
@@ -26,14 +26,8 @@ def test_client():
 
 
 @pytest.fixture
-def user_id():
-    return uuid4()
-
-
-@pytest.fixture
-def test_user(user_id):
+def test_user():
     new_user = {
-        "user_id": str(user_id),
         "username": "test_user",
         "email": "test_user@test_user.gov",
         "first_name": "test",
@@ -51,6 +45,8 @@ def test_create_user(test_client, test_user):
 
     new_user = response.json()
 
+    print(new_user)
+
     for k in test_user.keys():
         assert test_user[k] == new_user[k]
 
@@ -60,4 +56,9 @@ def test_get_user(test_client):
     test_id = 1
 
     response = test_client.get(f"/user/{test_id}")
+
     assert response.status_code == 200
+
+    retrieved_user = response.json()
+
+    print(retrieved_user)
